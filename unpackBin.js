91b017a34baedc300f7e2c9a331e3712bb299416
Joel Palmer <joelpalmer@gmail.com>
2019-02-21 11:08:51 -0600

    unpack a bin

---
+++ unpackBin.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const buffer = fs.readFileSync("./data/earthquakes.bin");
+
+const numRecords = buffer.readInt32LE(0);
+let bufferOffset = 4;
+const records = [];
+
+for (let recordIndex = 0; recordIndex < numRecords; ++recordIndex) {
+    const time = buffer.readDoubleLE(bufferOffset);
+
+    const record = {
+        Time: new Date(time),
+        Latitude: buffer.readDoubleLE(bufferOffset + 8),
+        Longitude: buffer.readDoubleLE(bufferOffset + 16),
+        Depth_Km: buffer.readDoubleLE(bufferOffset + 24),
+        Magnitude: buffer.readDoubleLE(bufferOffset + 32)
+    };
+
+    bufferOffset += 8 * 5;
+    records.push(record);
+}
+
+console.log(records);
