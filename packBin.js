e405750366637f24b95afaf6b6b9faaa7a34c745
Joel Palmer <joelpalmer@gmail.com>
2019-02-21 11:27:16 -0600

    add packBin

---
+++ packBin.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const moment = require("moment");
+
+const records = JSON.parse(fs.readFileSync("./data/earthquakes.json", "utf8"));
+
+const bufferSize = 4 + 8 * 5 * records.length;
+const buffer = new Buffer(bufferSize);
+
+buffer.writeInt32LE(records.length);
+
+let bufferOffset = 4;
+
+for (let i = 0; i < records.length; ++i) {
+	const record = records[i];
+	const time = moment(record.Time)
+		.toDate()
+		.getTime();
+
+	buffer.writeDoubleLE(time, bufferOffset);
+	bufferOffset += 8;
+	buffer.writeDoubleLE(record.Latitude, bufferOffset);
+	bufferOffset += 8;
+	buffer.writeDoubleLE(record.Longitude, bufferOffset);
+	bufferOffset += 8;
+	buffer.writeDoubleLE(record.Depth_Km, bufferOffset);
+	bufferOffset += 8;
+	buffer.writeDoubleLE(record.Magnitude, bufferOffset);
+	bufferOffset += 8;
+}
+
+fs.writeFileSync("./output/earthquakes.bin", buffer);
